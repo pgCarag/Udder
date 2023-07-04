e0f40caca5ee05e17c8737b87223823485f6a997
Prence Genesis Carag <prence_gen48@icloud.com>
2023-07-04 12:52:51 +0800

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Prence Genesis C. Carag on 7/4/23.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
